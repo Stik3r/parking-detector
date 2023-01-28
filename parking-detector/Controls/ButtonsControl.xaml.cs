d6afdad2247e14050cba70029fadd78892f96e98
Mahonin Matvey <maxon.07@mail.ru>
2023-01-28 11:43:25 +0300

    init commit

---
+++ parking-detector/Controls/ButtonsControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace parking_detector.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ButtonsControl.xaml
+    /// </summary>
+    public partial class ButtonsControl : UserControl
+    {
+        public VideoControl videoControl;
+        public ButtonsControl()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void LoadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string path = "";
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "Video Files |*.avi;*mp4;";
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                path = dialog.FileName;
+            }
+
+            videoControl.videoPlayer.Source = (Uri)(new UriTypeConverter().ConvertFromString(path));
+        }
+    }
+}
---
+++ parking-detector/Controls/VideoControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace parking_detector.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для VideoControl.xaml
+    /// </summary>
+    public partial class VideoControl : UserControl
+    {
+        private DispatcherTimer timerVideoPlayback;
+        public VideoControl()
+        {
+            InitializeComponent();
+        }
+
+        private void TimerVideoPlayback_Tick(object sender, object e)
+        {
+            long currentMediaTicks = videoPlayer.Position.Ticks;
+            long totalMediaTicks = videoPlayer.NaturalDuration.TimeSpan.Ticks;
+
+            if (totalMediaTicks > 0)
+                slider.Value = (double)currentMediaTicks / totalMediaTicks * 100;
+            else
+                slider.Value = 0;
+        }
+
+        private void VideoConrol_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            timerVideoPlayback = new DispatcherTimer();
+            timerVideoPlayback.Interval = TimeSpan.FromMilliseconds(10);
+            timerVideoPlayback.Tick += TimerVideoPlayback_Tick;
+            timerVideoPlayback.Start();
+        }
+
+        private void videoPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            timerVideoPlayback.Stop();
+        }
+    }
+}
